f0b3e90e16e3be42a9316cdf6cd774dc7019aeef
Dmytro Shumov <dmytro.shumov@chisw.com>
2024-06-10 14:09:24 +0300

    fix hw 7_3

--- hw_7_3/js/scripts.js
+++ hw_7_3/js/scripts.js
@@ -1,13 +1,13 @@
 function userDialog() {
     let countOfIteration = getRandomInt(3, 10);
     for(let i = 0; i < countOfIteration; i++) {
-        let userNumber = prompt("Enter number more that 100:")
+        let userNumber = prompt("Enter a number greater than 100:")
 
         if(isNotNumber(userNumber)) {
             console.log(`Not valid value ${userNumber}`);
         } else if(userNumber < 100) {
-            console.log(`Not valid number ${userNumber}. Enter number more that 100`);
-        } else {
+            console.log(`Not valid number ${userNumber}. Enter a number greater than 100`);
+        } else if(userNumber > 100) {
             console.log(userNumber);
             break;
         }
